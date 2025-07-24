23f645ddc9b5a79a419880599cd0f327df529953
rainbowdoor <100160529+rainbowdoor@users.noreply.github.com>
2025-07-25 02:19:04 +0600

    Update main.cpp

--- ConsoleApplication2/main.cpp
+++ ConsoleApplication2/main.cpp
@@ -3,33 +3,33 @@
 using namespace std;
 int main() {
 
-	//Спрашиваем вес
-	int weight;
+	//Г‘ГЇГ°Г ГёГЁГўГ ГҐГ¬ ГўГҐГ±
+	short weight;
 	cout << "What's your weight ?\n";
 	cin >> weight;
 
-	//Спрашиваем рост
-	int tall;
+	//Г‘ГЇГ°Г ГёГЁГўГ ГҐГ¬ Г°Г®Г±ГІ
+	short tall;
 	cout << "What is your height ?\n";
 	cin >> tall;
 
-	//Спрашиваем возраст
-	int age;
+	//Г‘ГЇГ°Г ГёГЁГўГ ГҐГ¬ ГўГ®Г§Г°Г Г±ГІ
+	short age;
 	cout << "How old are you ?\n";
 	cin >> age;
 
-	//Уточняем пол
-	int gender;
+	//Г“ГІГ®Г·Г­ГїГҐГ¬ ГЇГ®Г«
+	short gender;
 	cout << "What is your gender ? Set 1 for Male, set 2 for female\n";
 	std::cin >> gender;
 
-	// Формула для мужчин
+	// Г”Г®Г°Г¬ГіГ«Г  Г¤Г«Гї Г¬ГіГ¦Г·ГЁГ­
 	if (gender == 1) {
 		cout << "You need to eat every day " << 10 * weight + 6.25 * tall - 5 * age + 5 << " calories, including " << weight * 1.5 << " grams of protein per day\n";
 
 	}
 
-	//Формула для женщин
+	//Г”Г®Г°Г¬ГіГ«Г  Г¤Г«Гї Г¦ГҐГ­Г№ГЁГ­
 	if (gender == 2) {
 		cout << "You need to eat every day " << 10 * weight + 6.25 * tall - 5 * age - 161 << " calories, including " << weight * 1.3 << " grams of protein per day\n";
 
