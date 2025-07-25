732bbdd5046e4163e3d0c1c90ed9c4649700c3c3
RainbowDoor <shmonin27000@gmail.com>
2025-07-26 01:29:48 +0600

    откорректировал комментарии

--- ConsoleApplication2/main.cpp
+++ ConsoleApplication2/main.cpp
@@ -3,27 +3,27 @@
 using namespace std;
 int main() {
 
-	//Ñïðàøèâàåì âåñ
+	// ves
 	short weight;
 	cout << "What's your weight ?\n";
 	std::cin >> weight;
 
-	//Ñïðàøèâàåì ðîñò
+	//rost 
 	short tall;
 	cout << "What is your height ?\n";
 	std::cin >> tall;
 
-	//Ñïðàøèâàåì âîçðàñò
+	//vozrast
 	short age;
 	cout << "How old are you ?\n";
 	std::cin >> age;
 
-	//Óòî÷íÿåì ïîë
+	// gender
 	short gender;
 	cout << "What is your gender ? Set 1 for Male, set 2 for female\n";
 	std::cin >> gender;
 
-	// Ôîðìóëà äëÿ ìóæ÷èí
+	// uslovie
 	if (gender == 1) {
 		std::cout << "You need to eat every day " << 10 * weight + 6.25 * tall - 5 * age + 5 << " calories, including " << weight * 1.5 << " grams of protein per day\n";
 
@@ -32,7 +32,7 @@ int main() {
 		std::cout << "Repeat please";
 	}
 
-	//Ôîðìóëà äëÿ æåíùèí
+	// uslovie
 	if (gender == 2) {
 		std::cout << "You need to eat every day " << 10 * weight + 6.25 * tall - 5 * age - 161 << " calories, including " << weight * 1.3 << " grams of protein per day\n";
 
